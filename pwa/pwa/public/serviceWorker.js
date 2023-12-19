ae946830afe209b0f72eb8015eafc93036654332
ypriyam <ypriyam@corra.com>
2023-12-19 15:10:28 +0530

    updated the code

--- pwa/pwa/public/serviceWorker.js
+++ pwa/pwa/public/serviceWorker.js
@@ -1,5 +1,7 @@
 let CACHE_NAME = "my-site-cache-v1";
 const urlsToCache = ["/", "/index.html"];
+
+
 self.addEventListener("install", function (event) {
   // Perform install steps
   event.waitUntil(
@@ -13,15 +15,39 @@ self.addEventListener("install", function (event) {
 self.addEventListener("fetch", function (event) {
   event.respondWith(
     caches.match(event.request).then(function (response) {
+      // If the resource is found in the cache, return it
       if (response) {
         return response;
       }
-      if (event.request) {
-        return fetch(event.request);
-      }
+
+      // If the resource is not found in the cache, fetch it from the network
+      return fetch(event.request)
+        .then(function (networkResponse) {
+          // Check if the response is valid and if it's a successful response (status 200)
+          if (!networkResponse || networkResponse.status !== 200) {
+            return networkResponse; // You can customize this part based on your requirements
+          }
+
+          // Clone the response to store it in the cache
+          var responseToCache = networkResponse.clone();
+
+          // Open the cache and store the fetched response
+          caches.open(CACHE_NAME).then(function (cache) {
+            cache.put(event.request, responseToCache);
+          });
+
+          // Return the network response to the page
+          return networkResponse;
+        })
+        .catch(function (error) {
+          console.error("Fetch failed:", error);
+          // You can customize the fallback behavior here if the fetch fails
+          // For example, you can return a custom offline page
+        });
     })
   );
 });
+
 
 self.addEventListener("install", function (event) {
   // Perform install steps
