98451dc653f8689e6f36ae00639fde5cf7943ed9
ypriyam <ypriyam@corra.com>
2023-12-19 16:23:41 +0530

    Setup react PWA app

--- pwa/pwa/workbox-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    // Your Workbox configuration options go here
-    // For example:
-    globDirectory: 'public/',
-    globPatterns: ['**/*.{html,js,css}'],
-    swDest: 'public/sw.js',
-  };
