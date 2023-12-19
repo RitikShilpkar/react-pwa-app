4c3e488ce17513247ef05c9b52bbaa7dba96b1e5
ypriyam <ypriyam@corra.com>
2023-12-19 15:58:15 +0530

    updated the code

---
+++ pwa/pwa/workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // Your Workbox configuration options go here
+    // For example:
+    globDirectory: 'public/',
+    globPatterns: ['**/*.{html,js,css}'],
+    swDest: 'public/sw.js',
+  };
